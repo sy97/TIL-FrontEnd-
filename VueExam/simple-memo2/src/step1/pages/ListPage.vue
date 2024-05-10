fc6253b61dedf89631bdd4bba6f2ab4fcfa57eb5
Kang Soyeong <a44895575@gmail.com>
2024-05-10 16:25:34 +0900

    0510

---
+++ VueExam/simple-memo2/src/step1/pages/ListPage.vue
@@ -0,0 +1,25 @@
+<template>
+    <header-component title="리스트 페이지"></header-component> <!--전역 컴포넌트는 태그만 써줘도 알아서 import됨-->
+    <div>
+        리스트 컨텐츠
+        <ul>
+            <li>memo1</li>
+            <li>memo2</li>
+            <li>memo3</li>
+        </ul>
+    </div>
+    <footer-component></footer-component>
+</template>
+
+<script>
+
+    
+    export default {
+components: {  },
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
