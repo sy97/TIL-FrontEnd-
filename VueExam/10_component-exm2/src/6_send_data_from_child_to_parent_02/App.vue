4ef67599a612c2d1e43d566bc9f3e286471a3897
Kang Soyeong <a44895575@gmail.com>
2024-05-14 15:19:11 +0900

    0514

---
+++ VueExam/10_component-exm2/src/6_send_data_from_child_to_parent_02/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <my-button @myclk="log"> <!--이건 진짜 버튼이 아님. 진짜 이벤트도 아님. my-button은 자식컴포넌트임. @click도 자식에게 무슨일이 일어난건지 알려주는거임.
+  근데도 발생하는 이유는 , 자식컴포넌트에 상속돼서 자식컴포넌트에서 일어난것임.-->
+  <!--자식이 myclk로 이 이벤트가 발생했다는 것을 알려줬고, 그걸 부모가 받아와서 log라는 함수를 호출함 -->
+    MyButton
+  </my-button>
+</template>
+
+<script>
+
+import MyButton from './components/MyButton.vue'
+export default {
+  name: 'App',
+  components: {
+    MyButton
+  },
+  methods : {
+    log(info){ //myclk를 넘겨받아오면서 'message'를 받아옴. 그래서 'message'가 콘솔에 출력됨.
+      alert("Click!");
+      console.log(info);
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+} 
+</style>
---
+++ VueExam/10_component-exm2/src/6_send_data_from_child_to_parent_02/components/MyButton.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="btn">
+        <slot></slot> 
+    </div>
+    <h1 @click="$emit('myclk', 'message')">Test</h1> <!--이렇게 자식컴포넌트가 여러개가 있는경우에는, 상속을 어디에 줄지 몰라서 부모에서 적은 이벤트가 상속되지 않음.-->
+    <!--아래 에밋에 등록한 이름을 가져다가, 클릭이 발생했다는걸 부모에게 전달해줌.-->
+    <input type="text" /> 
+</template>
+
+<script>
+    export default {
+        emits : ["myclk"] //부모에게 클릭이라는 이벤트가 발생했다는것을 알려줌.
+    }
+</script>
+
+<style scoped>
+    .btn {
+        display : inline-block;
+        margin: 4px;
+        padding: 6px 12px;
+        border-radius: 4px;
+        background-color: gray;
+        color: white;
+        cursor: pointer;
+    }
+</style>
